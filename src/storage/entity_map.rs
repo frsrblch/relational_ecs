508f24010beeb922f5ca26f517433586c4b2381d
Fraser Balch <farseer.ulwithy@gmail.com>
2019-12-09 00:49:40 -0700

    exposed HashMap as values

--- src/storage/entity_map.rs
+++ src/storage/entity_map.rs
@@ -2,11 +2,11 @@ use super::*;
 use rustc_hash::FxHashMap;
 
 #[derive(Debug, Clone)]
-pub struct EntityMap<ID: IdType, T>(FxHashMap<ID, T>);
+pub struct EntityMap<ID: IdType, T> { pub values: FxHashMap<ID, T> }
 
 impl<ID: IdType, T> Default for EntityMap<ID, T> {
     fn default() -> Self {
-        EntityMap(Default::default())
+        EntityMap { values: Default::default() }
     }
 }
 
@@ -16,36 +16,36 @@ impl<ID: IdType, T> EntityMap<ID, T> {
     }
 
     pub fn iter(&self) -> std::collections::hash_map::Iter<ID, T> {
-        self.0.iter()
+        self.values.iter()
     }
 
     pub fn clear(&mut self) {
-        self.0.clear();
+        self.values.clear();
     }
 
     pub fn insert(&mut self, id: &VerifiedEntity<ID>, value: T) {
-        self.0.insert(id.entity, value);
+        self.values.insert(id.entity, value);
     }
 
     pub fn remove(&mut self, id: &VerifiedEntity<ID>) -> Option<T> {
-        self.0.remove(&id.entity)
+        self.values.remove(&id.entity)
     }
 
     pub fn len(&self) -> usize {
-        self.0.len()
+        self.values.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
+        self.values.is_empty()
     }
 }
 
 impl<ID: IdType, T> Get<ID, T> for EntityMap<ID, T> {
     fn get(&self, id: &VerifiedEntity<ID>) -> Option<&T> {
-        self.0.get(&id.entity)
+        self.values.get(&id.entity)
     }
 
     fn get_mut(&mut self, id: &VerifiedEntity<ID>) -> Option<&mut T> {
-        self.0.get_mut(&id.entity)
+        self.values.get_mut(&id.entity)
     }
 }
