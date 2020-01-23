1ba23d497258a1a1c24119d0126fac04f9f55dab
Fraser Balch <farseer.ulwithy@gmail.com>
2020-01-22 19:43:49 -0700

    removed duplicate traits

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,6 @@
 pub use crate::id_type;
 pub use crate::entities::{VerifiedEntity, Allocator};
-pub use crate::traits::*;
+pub use crate::traits::{Split, Update};
 pub use crate::storage::*;
 pub use crate::traits_new::*;
 pub use crate::allocators::*;
