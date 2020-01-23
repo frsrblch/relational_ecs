d7d4a1bbe5122c51ea366d73fcee404a2f0b7248
Fraser Balch <farseer.ulwithy@gmail.com>
2020-01-22 19:40:59 -0700

    updated prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,3 +2,6 @@ pub use crate::id_type;
 pub use crate::entities::{VerifiedEntity, Allocator};
 pub use crate::traits::*;
 pub use crate::storage::*;
+pub use crate::traits_new::*;
+pub use crate::allocators::*;
+pub use crate::ids::*;
